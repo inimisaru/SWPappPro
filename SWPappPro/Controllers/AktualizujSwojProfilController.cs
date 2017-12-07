c70ddf96c152b6b4b216dfe5af5cadd018d6f939
Bartłomiej Balak <bartlomiej.balak@student.wat.edu.pl>
2017-12-07 16:11:45 +0100

    Opracowanie rozwiązania dla UC: AktualizujSwojProfil

--- SWPappPro/Controllers/AktualizujSwojProfilController.cs
+++ SWPappPro/Controllers/AktualizujSwojProfilController.cs
@@ -11,29 +11,44 @@ using SWPappPro.Models;
 
 namespace SWPappPro.Controllers
 {
+    /// <summary>
+    /// Kontroler dla aktualizacji profilu lekarza
+    /// </summary>
     public class AktualizujSwojProfilController : Controller
     {
+        /// <summary>
+        /// Nowy obiekt SWPappDBEntities 
+        /// </summary>
         private SWPappDBEntities db = new SWPappDBEntities();
-        // GET: AktualizujSwojProfil
-        public ActionResult Index()
-        {
-            return View();
-        }
+        /// <summary>
+        /// Metoda wyswietlajaca widok z formualarzem
+        /// W formatkach znajduja sie dane pobrane z bazy danych odpowiadajace ID pobranemu ze zmiennych sesji
+        /// </summary>
+        /// <returns></returns>
         public ActionResult AktualizujSwojProfil()
         {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult AktualizujSwojProfilZatwierdz([Bind(Include = "LEKARZ_ID,SPECJALIZACJA,NR_LICENCJI,HASLO")] LEKARZ lEKARZ)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(lEKARZ).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+            int id = (int) Session["id"];
+            LEKARZ lEKARZ = db.LEKARZ.Find(id);
             return View(lEKARZ);
         }
-
+        /// <summary>
+        /// Metoda wywolywana po wprowadzeniu zmian
+        /// </summary>
+        /// <param name="lEKARZ"></param> obiekt typu LEKARZ z wprowadzonymi zmianiami
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult AktualizujSwojProfilZatwierdz([Bind(Include = "LEKARZ_ID,IMIE,NAZWISKO,SPECJALIZACJA,PESEL,DATA_URODZENIA,NR_LICENCJI,HASLO")] LEKARZ lEKARZ)
+        {
+                if (ModelState.IsValid)
+                {
+                    //oznacz jako zmienione
+                    db.Entry(lEKARZ).State = EntityState.Modified;
+                    //zapisz zmiany
+                    db.SaveChanges();
+                    //zwrócenie widoku AktualizujSwojProfil
+                    return View("AktualizujSwojProfilWynik");
+                }
+                return View("AktualizujSwojProfil");
+        }
     }
 }
--- SWPappPro/Models/LEKARZ.cs
+++ SWPappPro/Models/LEKARZ.cs
@@ -13,9 +13,14 @@ namespace SWPappPro.Models
     using System.Collections.Generic;
     using System.Web;
     using System.Linq;
-
+    /// <summary>
+    /// Klasa modelu danych odpowiadaj¹ca za mapujaca na encje tablicy LEKARZ
+    /// </summary>
     public partial class LEKARZ
     {
+        /// <summary>
+        /// Konstruktor klasy LEKARZ
+        /// </summary>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public LEKARZ()
         {
@@ -28,14 +33,37 @@ namespace SWPappPro.Models
             this.WIZYTA_DOMOWA = new HashSet<WIZYTA_DOMOWA>();
             this.WIZYTA_KONSULTACYJNA = new HashSet<WIZYTA_KONSULTACYJNA>();
         }
-    
+        /// <summary>
+        /// Pararametr LEKARZ_ID
+        /// </summary>
         public int LEKARZ_ID { get; set; }
+        /// <summary>
+        /// Pararametr IMIE
+        /// </summary>
         public string IMIE { get; set; }
+        /// <summary>
+        /// Pararametr NAZWISKO
+        /// </summary>
         public string NAZWISKO { get; set; }
+        /// <summary>
+        /// Pararametr SPECJALIZACJA
+        /// </summary>
         public string SPECJALIZACJA { get; set; }
+        /// <summary>
+        /// Pararametr PESEL
+        /// </summary>
         public string PESEL { get; set; }
+        /// <summary>
+        /// Pararametr DATA_URODZENIA
+        /// </summary>
         public Nullable<System.DateTime> DATA_URODZENIA { get; set; }
+        /// <summary>
+        /// Pararametr NR_LICENCJI
+        /// </summary>
         public string NR_LICENCJI { get; set; }
+        /// <summary>
+        /// Pararametr HASLO
+        /// </summary>
         public string HASLO { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
@@ -55,6 +83,12 @@ namespace SWPappPro.Models
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<WIZYTA_KONSULTACYJNA> WIZYTA_KONSULTACYJNA { get; set; }
 
+        /// <summary>
+        /// Metoda odpowiadaj¹ca za walidacje logowania do systemu jako lekarz
+        /// </summary>
+        /// <param name="entity"></param> Obiekt z wprowadzonymi przez uzytkownika danymi podlegaj¹cymi sprawdzeniu
+        /// <param name="current"></param>Obiekt przechowuj¹cy odwo³onie do sesji u¿ytkownika
+        /// <returns></returns>
         public bool ZalogujLekarz(LEKARZ entity, HttpContextBase current)
         {
 
